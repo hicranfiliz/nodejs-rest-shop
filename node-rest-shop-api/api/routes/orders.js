b4c7255cc5d84a5a54b2918a782cc9fe9e688361
Hicran Filiz <h.filiz781@gmail.com>
2022-12-13 18:51:11 +0300

    feat:populat

--- node-rest-shop-api/api/routes/orders.js
+++ node-rest-shop-api/api/routes/orders.js
@@ -8,6 +8,7 @@ const Product = require("../models/product")
 router.get('/', (req, res) => {
     Order.find()
         .select('product quantity _id')
+        .populate('product', 'name')
         .exec()
         .then(docs => {
             res.status(200).json({
@@ -73,6 +74,7 @@ router.post('/', (req, res, next) => {
 
 router.get('/:orderId', (req, res) => {
     Order.findById(req.params.orderId)
+        .populate('product')
         .exec()
         .then(order => {
             if (!order) {
